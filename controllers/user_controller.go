d9c3ed9ee2f596bd02bb0e15fe8272d90d31ac4e
Oleksandr <malex307ander@gmail.com>
2025-01-22 23:37:45 +0200

    Feat: Implement JWT authentication and user password management
    - Created `auth/jwt.go` to handle JWT functionality:
      - Added `GenerateToken` for generating JWTs with user-specific claims (`user_id`, `email`, `name`).
      - Added `ValidateToken` to parse and verify JWTs, returning the authenticated user.
    
    - Created `helpers/hash_helper.go` for password handling:
      - Added `HashPassword` to securely hash plain-text passwords using `bcrypt`.
      - Added `CheckPassword` to compare plain-text passwords with hashed versions.
    
    - Updated `controllers/user_controller.go`:
      - Enhanced `CreateUser` to hash passwords during user creation and return a JWT token.
      - Added `GetMe` endpoint to fetch user details based on the provided JWT.
    
    - Updated `models/user.go`:
      - Added `Password` field to store hashed passwords for users.
    
    - Updated `routes/api.go`:
      - Registered new `/user/me` endpoint for authenticated user data retrieval.
    
    - Updated `go.mod` and `go.sum`:
      - Added dependencies: `github.com/golang-jwt/jwt/v5`, `golang.org/x/crypto`, and `github.com/joho/godotenv`.

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
+	"BudgetApp/auth"
 	"BudgetApp/helpers"
 	"BudgetApp/models"
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -25,10 +27,36 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	user.Password, err = helpers.HashPassword(user.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 	if err := db.Create(&user).Error; err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	authToken, err := auth.GenerateToken(user)
+
+	helpers.NewResponse(w).ResponseJSON(map[string]any{"user": user, "token": authToken})
+}
+
+func GetMe(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Get a specific header
+	authHeader := r.Header.Get("Authorization")
+
+	// If you're specifically looking for the JWT token from Authorization header
+	// It's typically sent as "Bearer <token>"
+	authHeader = r.Header.Get("Authorization")
+	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+	user, _ := auth.ValidateToken(tokenString)
 
 	helpers.NewResponse(w).ResponseJSON(user)
 }
--- models/user.go
+++ models/user.go
@@ -4,7 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	ID    uint `gorm:"primaryKey"`
-	Name  string
-	Email string `gorm:"unique"`
+	ID       uint `gorm:"primaryKey"`
+	Name     string
+	Email    string `gorm:"unique"`
+	Password string
 }
