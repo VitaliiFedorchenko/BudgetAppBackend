92780bc24fdf2abff2cef8e549bea364a20033b0
Oleksandr <malex307ander@gmail.com>
2025-01-28 19:57:47 +0200

    test

--- internal/configs/mysql.go
+++ internal/configs/mysql.go
@@ -1,21 +1,13 @@
 package configs
 
 import (
-	"BudgetApp/cmd/server/logger"
 	"fmt"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"os"
 )
 
 func ConnectionToDataBase() (*gorm.DB, error) {
-
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println("Warning: Could not load .env file, relying on system environment variables")
-	}
-
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
@@ -23,7 +15,6 @@ func ConnectionToDataBase() (*gorm.DB, error) {
 		os.Getenv("DB_NAME"),
 	)
 
-	logger.LogInfo(dsn)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
